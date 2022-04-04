cbe20fd9cfd225561163cf9633d60d1f055bd36c
Aung Ko Min <jonahaung@gmail.com>
2022-04-05 06:05:22 +0800

    Initial Commit

---
+++ SwiftUICamera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICamera
+//
+//  Created by Aung Ko Min on 5/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICamera/SwiftUICameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICameraApp.swift
+//  SwiftUICamera
+//
+//  Created by Aung Ko Min on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
